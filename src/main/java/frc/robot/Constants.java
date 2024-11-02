3e3b2d8db631c8945e723dc3aa5713c2b521a6d1
Lily L <lily.lietzau@hopkinsrobotics.org>
2024-11-02 13:58:34 -0500

    Changed CANID of the motor

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -18,7 +18,7 @@ public final class Constants {
     public static final int rightFollowerCANID = 5;
     public static final int leftLeaderCANID = 7;
     public static final int leftFollowerCANID = 8;
-    public static final int mainMechanismCANID = 3;
+    public static final int mainMechanismCANID = 10;
     //The controller ports can be changed in driverstation
     public static final int driverXboxControllerPort = 0;
     public static final int operatorXboxControllerPort = 1;
