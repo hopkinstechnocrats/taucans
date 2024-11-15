1e7ece42612560d47948ea85410eaaf6d2e4e65a
Andre Nasses <Andre.Nasses@hopkinsrobotics.org>
2024-11-14 19:32:26 -0600

    breaktime commit

--- src/main/java/frc/robot/auto/autoRoutines.java
+++ src/main/java/frc/robot/auto/autoRoutines.java
@@ -1,6 +1,7 @@
 package frc.robot.auto;
 
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import frc.robot.Constants;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -19,7 +20,7 @@ public class autoRoutines{
     public Command rotateClockwise45 =        DriveCommands.drive(driveSubsystem, -1, 1).withTimeout(1);
     public Command rotateCounterclockwise45 = DriveCommands.drive(driveSubsystem, 1, -1).withTimeout(1);
     public Command moveForward(double time) {return DriveCommands.drive(driveSubsystem, 1, 1).withTimeout(1);};
-    public Command launchBall = LauncherCommands.spinLauncher(null);
+    public Command launchBall() {return LauncherCommands.spinLauncher(null).withTimeout(1);};
 
     public Command auto_left_hook = new SequentialCommandGroup(
         moveForward(1),
@@ -51,16 +52,51 @@ public class autoRoutines{
 
     public Command auto_straight_launch = new SequentialCommandGroup(
         moveForward(2),
-        spinLauncher()
+        launchBall()
+    );
+
+    public Command auto_right_curve_launch = new SequentialCommandGroup(
+        moveForward(1),
+        rotateClockwise45,
+        moveForward(1),
+        rotateCounterclockwise45,
+        launchBall()
+    );
+
+    public Command auto_left_curve_launch = new SequentialCommandGroup(
+        moveForward(1),
+        rotateCounterclockwise45,
+        moveForward(1),
+        rotateClockwise45,
+        launchBall()
+    );
+
+     public Command auto_left_hook_launch = new SequentialCommandGroup(
+        moveForward(1),
+        rotateCounterclockwise90,
+        moveForward(1),
+        rotateClockwise90,
+        launchBall()
+    );
+
+     public Command auto_right_hook_launch = new SequentialCommandGroup(
+        moveForward(1),
+        rotateClockwise90,
+        moveForward(1),
+        rotateCounterclockwise90,
+        launchBall()
     );
 
    public Command selectAuto(){
         autoChooser.setDefaultOption("Straight", auto_straight);
-        autoChooser.setDefaultOption("Straight Shoot", auto_straight_launch);
+        autoChooser.setDefaultOption("Straight Launch", auto_straight_launch);
+        autoChooser.addOption("Right Hook", auto_right_hook);
         autoChooser.addOption("Right Hook", auto_right_hook);
         autoChooser.addOption("Right Curve", auto_right_curve);
+        autoChooser.addOption("Right Curve Launch", auto_right_curve_launch);
         autoChooser.addOption("Left Hook", auto_left_hook);
         autoChooser.addOption("Left Curve", auto_left_curve);
+        autoChooser.addOption("Left Curve Launch", auto_left_curve_launch);
         SmartDashboard.putData("Are you starting on the left or right?", autoChooser);
 
 
