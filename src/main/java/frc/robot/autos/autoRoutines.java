8d5b5356d059eeb79628e04b6811d96a04b46753
Lily <lily.lietzau@hopkinsrobotics.org>
2025-11-04 20:17:14 -0600

    autos is actually java code now yay

--- src/main/java/frc/robot/autos/autoRoutines.java
+++ src/main/java/frc/robot/autos/autoRoutines.java
@@ -1,4 +1,4 @@
-package frc.robot.auto
+package frc.robot.autos;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
